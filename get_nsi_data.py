70f7c1f76c99938396d52eeb1ddb6fc305d9d89c
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2022-12-23 13:08:48 -0500

    complete statesDict
    Add remaining states, abbreviations, and fips codes to statesDict

--- get_nsi_data.py
+++ get_nsi_data.py
@@ -214,7 +214,7 @@ class GetNSIData:
                 if reply.error() != None and reply.error() != QNetworkReply.NoError:
                     self.is_error = reply.errorString()
                     reply.abort()
-                    reply.deleteLater()
+                    reply.deleteLater() # *** What does this do?
                         
                 elif reply.error() ==  QNetworkReply.NoError:
                     result = reply.readAll()
@@ -231,14 +231,14 @@ class GetNSIData:
                     except:
                         pass
                         
-                    self.set_progress()
+                    
                     self.iface.messageBar().pushMessage(
                         "Success", f"Downloaded file saved at {self.dir}",
                         level=Qgis.Success, duration=5
                     )
                         
                 # Clean up. */
-                    reply.deleteLater()
+                    reply.deleteLater()# *** What does this do?
                     
     def layer_exists(self,  name):            
         
@@ -286,11 +286,60 @@ class GetNSIData:
             #self.first_start = False
             self.dlgState = GetStateNSIDataDialog()
             self.dlgState.stateFolderButton.clicked.connect(self.selectStateOutputFolder)
-
+        
+        # In future, move this to external file and load in
         statesDict = {
             'Alabama': {'abbr':'AL', 'fips':'01'},
             'Alaska': {'abbr':'AK', 'fips':'02'},
-            'Arizona': {'abbr':'AZ', 'fips':'04'}
+            'Arizona': {'abbr':'AZ', 'fips':'04'},
+            'Arkansas': {'abbr':'AR', 'fips':'05'},
+            'California': {'abbr':'CA', 'fips':'06'},
+            'Colorado': {'abbr':'CO', 'fips':'08'},
+            'Connecticut': {'abbr':'CT', 'fips':'09'},
+            'Delaware': {'abbr':'DE', 'fips':'10'},
+            'District of Columbia': {'abbr':'DC', 'fips':'11'},
+            'Florida': {'abbr':'FL', 'fips':'12'},
+            'Georgia': {'abbr':'GA', 'fips':'13'},
+            'Hawaii': {'abbr':'HI', 'fips':'15'},
+            'Idaho': {'abbr':'ID', 'fips':'16'},
+            'Illinois': {'abbr':'IL', 'fips':'17'},
+            'Indiana': {'abbr':'IN', 'fips':'18'},
+            'Iowa': {'abbr':'IA', 'fips':'19'},
+            'Kansas': {'abbr':'KS', 'fips':'20'},
+            'Kentucky': {'abbr':'KY', 'fips':'21'},
+            'Louisiana': {'abbr':'LA', 'fips':'22'},
+            'Maine': {'abbr':'ME', 'fips':'23'},
+            'Maryland': {'abbr':'MD', 'fips':'24'},
+            'Massachusetts': {'abbr':'MA', 'fips':'25'},
+            'Michigan': {'abbr':'MI', 'fips':'26'},
+            'Minnesota': {'abbr':'MN', 'fips':'27'},
+            'Mississippi': {'abbr':'MS', 'fips':'28'},
+            'Missouri': {'abbr':'MO', 'fips':'29'},
+            'Montana': {'abbr':'MT', 'fips':'30'},
+            'Nebraska': {'abbr':'NE', 'fips':'31'},
+            'Nevada': {'abbr':'NV', 'fips':'32'},
+            'New Hampshire': {'abbr':'NH', 'fips':'33'},
+            'New Jersey': {'abbr':'NJ', 'fips':'34'},
+            'New Mexico': {'abbr':'NM', 'fips':'35'},
+            'New York': {'abbr':'NY', 'fips':'36'},
+            'North Carolina': {'abbr':'NC', 'fips':'37'},
+            'North Dakota': {'abbr':'ND', 'fips':'38'},
+            'Ohio': {'abbr':'OH', 'fips':'39'},
+            'Oklahoma': {'abbr':'OK', 'fips':'40'},
+            'Oregon': {'abbr':'OR', 'fips':'41'},
+            'Pennsylvania': {'abbr':'PA', 'fips':'42'},
+            'Rhode Island': {'abbr':'RI', 'fips':'44'},
+            'South Carolina': {'abbr':'SC', 'fips':'45'},
+            'South Dakota': {'abbr':'SD', 'fips':'46'},
+            'Tennessee': {'abbr':'TN', 'fips':'47'},
+            'Texas': {'abbr':'TX', 'fips':'48'},
+            'Utah': {'abbr':'UT', 'fips':'49'},
+            'Vermont': {'abbr':'VT', 'fips':'50'},
+            'Virginia': {'abbr':'VA', 'fips':'51'},
+            'Washington': {'abbr':'WA', 'fips':'53'},
+            'West Virginia': {'abbr':'WV', 'fips':'54'},
+            'Wisconsin': {'abbr':'WI', 'fips':'55'},
+            'Wyoming': {'abbr':'WY', 'fips':'56'}
         }
         
         # Clear the contents of the state comboBox from previous runs
